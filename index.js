a182763c78e137d954c1eb085af0d909efe4b798
Daniel Marino <starzonmyarmz@gmail.com>
2021-01-22 11:08:47 -0500

    all the things

---
+++ index.js
@@ -0,0 +1,14 @@
+const alfy = require('alfy')
+
+const gifz = 'https://gifz.netlify.app/'
+const data = await alfy.fetch(`${gifz}gifs.json`)
+
+const items = alfy
+	.inputMatches(data, 'keywords')
+	.map(element => ({
+		title: element.keywords,
+		subtitle: element.url,
+    arg: gifz + element.url
+	}))
+
+alfy.output(items)
